3e591b9940e63300dec797e54b045f84b6e0cc30
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-01 09:55:28 +0800

    Update

--- admin/report-overview.php
+++ admin/report-overview.php
@@ -1,3 +1,51 @@
+<?php 
+require_once '../config/config.php';
+session_start();
+
+$paymentDetails = [];
+$received_id = null;
+
+function generateModalData($received_id) {
+    global $conn;
+
+    // Sanitize the received ID (for example, using mysqli_real_escape_string)
+    $sanitized_id = intval($received_id);
+    $sql = "SELECT * FROM monthly_payment_details WHERE id='$sanitized_id'";
+    $query_result = $conn->query($sql);
+    $query_data = array();
+    while ($row = $query_result->fetch_assoc()) {
+        $query_data[] = $row;
+    }
+    
+    return $query_data;
+}  
+
+
+function getPaymentData()
+{
+    global $conn;
+    $sql = "SELECT * FROM monthly_payment_details";
+    $result = $conn->query($sql);
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return $data;
+}         
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id'])) {
+    global $received_id;
+    $receivedId = $_POST['id'];
+
+    $received_id = $receivedId;
+    
+}
+
+
+$paymentData = getPaymentData();
+$paymentDetails = generateModalData($received_id);
+?>
 <!DOCTYPE html>
 <html lang = "en">
     <head>
@@ -35,36 +83,14 @@
             margin: 5px;
         }
 
-        /* Styling for modal */
-        .modal {
-        display: none; /* Hide modal by default */
-        position: fixed;
-        z-index: 1;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgba(0,0,0,0.5); /* Semi-transparent background */
+        .scroll{
+            overflow-y: scroll;
         }
-        .modal-content {
-        background-color: #fff;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 80%;
-        }
-        .close {
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-        }
-        .close:hover,
-        .close:focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
+
+        .remarks{
+            height: 150px;
+            border-radius: 10px;
+            width: 100%
         }
     </style>
     </head>
@@ -72,83 +98,130 @@
         <button type="button" class="btn btn-success" onclick="BackPage('payment_reports.php')">Back</button>
         <div class="container" style="margin-top:5%;">
             <h2 class="mt-4">Edit Report</h2>
-            <?php
-                require_once '../config/config.php';
-                session_start();
-
-                if (isset($_GET['month'])) {
-                    // Get the 'id' parameter value from the URL
-                    $id = $_GET['month'];
-                    
-                    $sql = "SELECT * FROM monthly_payment_details WHERE month='$id'";
-                    $result = $conn->query($sql);
-                    if ($result->num_rows > 0) {
-                        // Output data of each row
-                        echo "<table border='1'>";
-                        echo "<tr><th>ID</th>";
-                        echo "<th>Fullname</th>";
-                        echo "<th>Monthly Rental</th>";
-                        echo "<th>Extension Rental</th>";
-                        echo "<th>Stall Extension Fee</th>";
-                        echo "<th>Penalty</th>";
-                        echo "<th>Interest</th>";
-                        echo "<th>OR Number</th>";
-                        echo "<th>Date</th>";
-                        echo "<th>Total Amount</th>";
-                        echo "<th>Remarks</th>";
-                        echo "<th>Status</th>";
-                        echo "<th>Stall Number</th>";
-                        echo "<th>Action</th></tr>";
-                    
-                        while ($row = $result->fetch_assoc()) {
-                            echo "<tr><td>" . $row["id"] . "</td>";
-                            echo "<td>". $row["fullname"] . "</td>";
-                            echo "<td>". $row["monthly_rental"] . "</td>";
-                            echo "<td>". $row["extension_rental"] . "</td>";
-                            echo "<td>". $row["stall_extension_fee"] . "</td>";
-                            echo "<td>". $row["penalty_25"] . "</td>";
-                            echo "<td>". $row["interest_2"] . "</td>";
-                            echo "<td>" . $row["or_no"] . "</td>";
-                            echo "<td>" . $row["date"] . "</td>";
-                            echo "<td>" . $row["total_amount"] . "</td>";
-                            echo "<td>" . $row["remarks"] . "</td>";
-                            echo "<td>" . $row["status"] . "</td>";
-                            echo "<td>" . $row["owner_id"] . "</td>";
-                            echo "<td>" . "<button class= 'btn btn-success' onclick=openModal(" . $row["id"] .")>Edit</button>" . "<button class= 'btn btn-success'>Delete</button>" ."</td></tr>";
-                        }
-                    
-                        echo "</table>";
-                    } else {
-                        echo "<h2>No Table Found</h2>";
-                    }
-                } else {
-                    echo "ID parameter is missing.";
-                }
-            ?>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <!--<table border='1'>--->
+                            <th>ID</th>
+                            <th>Fullname</th>
+                            <th>Monthly Rental</th>
+                            <th>Extension Rental</th>
+                            <th>Stall Extension Fee</th>
+                            <th>Penalty</th>
+                            <th>Interest</th>
+                            <th>OR Number</th>
+                            <th>Date</th>
+                            <th>Total Amount</th>
+                            <th>Remarks</th>
+                            <th>Status</th>
+                            <th>Stall Number</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($paymentData as $payment) : ?>
+                        <?php 
+                            require_once '../config/config.php';
+                            if (isset($_GET['month'])) {
+                                // Get the 'id' parameter value from the URL
+                                $id = $_GET['month'];
+                                $sql = "SELECT * FROM monthly_payment_details WHERE month='$id'";
+                                $result = mysqli_query($conn, $sql);
+                            }
+                        ?>
+                            <tr>
+                                <td><?php echo $payment['id']; ?></td>
+                                <td><?php echo $payment['fullname']; ?></td>
+                                <td><?php echo $payment['monthly_rental']; ?></td>
+                                <td><?php echo $payment['extension_rental']; ?></td>
+                                <td><?php echo $payment['stall_extension_fee']; ?></td>
+                                <td><?php echo $payment['penalty_25']; ?></td>
+                                <td><?php echo $payment['interest_2']; ?></td>
+                                <td><?php echo $payment['or_no']; ?></td>
+                                <td><?php echo $payment['date']; ?></td>
+                                <td><?php echo $payment['total_amount']; ?></td>
+                                <td><?php echo $payment['remarks']; ?></td>
+                                <td><?php echo $payment['status']; ?></td>
+                                <td><?php echo $payment['owner_id']; ?></td>
+                                <td>
+                                    <button onclick="openModal()" class="edit-button btn btn-primary" data-value="<?php echo $payment['id']; ?>">Edit</button>                   
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div id="myModal" class="modal">
-            <!-- Modal content -->
-            <div class="modal-content">
-                <span class="close" onclick="closeModal()">&times;</span>
-                <div id="modalContent">
-                    <form method="POST">
-                        <label for="flname">Fullname: </label>
-                        <input id= "flname" readonly><br>
-                        <label for="monrent">Monthly Rental: </label>
-                        <input id= "monrent"><br>
-                        <label for="extrent">Extension Rental: </label>
-                        <input id= "extrent" ><br>
-                        <label for="stall-fee">Stall Extension Fee: </label>
-                        <input id= "stall-fee" ><br>
-                        <label for="penalty">Penalty: </label>
-                        <input id= "penalty" ><br>
-                        <label for="rental">Rental: </label>
-                        <input id= "rental" ><br>
-                    </form>
+        <div class="modal scroll" id="myModal" tabindex="-1" role="dialog" aria-labelledby="adminLoginModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="adminLoginModalLabel">Information Editor</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" onclick="closeModal()">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <?php foreach ($paymentDetails as $payment) : ?>
+                            <form method="POST">
+                                <div class="form-group">
+                                    <label for="flname">Full Name: </label>
+                                    <input id= "flname" class="form-control" value="<?php echo $payment['fullname']; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="monrent">Monthly Rental: </label>
+                                    <input id= "monrent" class="form-control" name="monthly_rental">
+                                </div>
+                                <div class="form-group">
+                                    <label for="extrent">Extension Rental: </label>
+                                    <input id= "extrent" class="form-control" name="extension_rental">
+                                </div>
+                                <div class="form-group">
+                                    <label for="stall-fee">Stall Extension Fee: </label>
+                                    <input id= "stall-fee" class="form-control" name="stall_extension_fee">
+                                </div>
+                                <div class="form-group">
+                                    <label for="penalty">Penalty: </label>
+                                    <input id= "penalty" class="form-control" name="penalty">
+                                </div>
+                                <div class="form-group">
+                                    <label for="interest">Interest: </label>
+                                    <input id= "interest" class="form-control" name="interest">
+                                </div>
+                                <div class="form-group">
+                                    <label for="or_num">OR Number: </label>
+                                    <input id= "or_num" class="form-control" value="<?php echo $payment['or_no']; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="date">Date: </label>
+                                    <input id= "date" class="form-control" value="<?php echo $payment['date']; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="total-amount">Total Amount: </label>
+                                    <input id= "total-amount" class="form-control" value="<?php echo $payment['total_amount']; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="remarks">Remarks: </label><br>
+                                    <textarea type="textarea" class="remarks" id= "remarks" placeholder="Insert a Remarks"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="status">Status: </label>
+                                    <input id= "status" class="form-control" value="<?php echo $payment['status']; ?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="stall-number">Stall Number: </label>
+                                    <input id= "stall-number" class="form-control" value="<?php echo $payment['owner_id']; ?>" readonly>
+                                </div>
+                                <button type="submit" class="btn btn-primary" name="save_changes">Save Changes</button>
+                            </form>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
         </div>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script>
             function BackPage(url){
                 window.location.href=url;
@@ -159,8 +232,6 @@
             // Function to open the modal
             function openModal(id) {
                 modal.style.display = "block";
-                // You can use AJAX with PHP to fetch dynamic content here
-                // For simplicity, it's empty in this example
             }
 
             // Function to close the modal
@@ -174,6 +245,31 @@
                 modal.style.display = "none";
                 }
             }
+
+            document.addEventListener('DOMContentLoaded', function() {
+                var buttons = document.querySelectorAll('.edit-button');
+
+                buttons.forEach(function(button) {
+                    button.addEventListener('click', function() {
+                        var idValue = this.getAttribute('data-value');
+                        
+                        // AJAX request to send the 'idValue' to a PHP script
+                        $.ajax({
+                            type: "POST",
+                            url: "report-overview.php",
+                            data: { id: idValue }, // Send the 'idValue' as 'id' in POST data
+                            success: function(response) {
+                                console.log("Data sent successfully to PHP.");
+                                // Handle the response from PHP if needed
+                                // console.log("PHP Response: ", response);
+                            },
+                            error: function(xhr, status, error) {
+                                console.error("Error sending data to PHP:", error);
+                            }
+                        });
+                    });
+                });
+            });
         </script>
     </body>
 </html>
