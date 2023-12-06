d4c1bb01e37414545c0be04d5541c2b221c1f737
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-07 02:09:44 +0800

    CSV File Generation

--- admin/generate_report_pdf.php
+++ admin/generate_report_pdf.php
@@ -1,52 +1,42 @@
 <?php
 include("../config/config.php");
-include("../tcpdf/tcpdf.php");
 session_start();
 
 
-if (isset($_GET["month"])){
-    // Fetch data from MySQL
+if (isset($_GET["month"])){  
+    // SQL query to retrieve data from the database
     $month = $_GET["month"];
     $sql = "SELECT * FROM monthly_payment_details WHERE month='$month'";
     $result = $conn->query($sql);
 
-    // Create new PDF document
-    $pdf = new TCPDF();
+    // Create a new file pointer
+    $file = fopen('data.csv', 'w');
 
-    // Set document information and properties
-    $pdf->SetCreator(PDF_CREATOR);
-    $pdf->SetTitle('MySQL Data to PDF');
-    $pdf->SetHeaderData('', 0, 'Monthly Report for the Month of ' . $month, '');
+    // Set custom header
+    $header = ["ID","Monthly Rental","Extension rental","Stall Extension Fee","Penalty","Interest","OR Number","Date","Total Amount","Remarks","Status","Stall Number","Month", "Fullname"];
+    // Customize this array with your desired header names
+    fputcsv($file, $header);
 
-    // Add a page
-    $pdf->AddPage();
-
-    // Set font
-    $pdf->SetFont('helvetica', '', 12);
-
-    // Iterate through fetched data and add to PDF
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $pdf->Write(0, "ID: " . strval($row['id']) . "\n");
-            $pdf->Write(0, "Monthly Rental: " . strval($row['monthly_rental']) . "\n");
-            $pdf->Write(0, "Extension Rental: " . strval($row['extension_rental']) . "\n");
-            $pdf->Write(0, "Stall Extension Fee: " . strval($row['stall_extension_fee']) . "\n");
-            $pdf->Write(0, "Penalty: " . strval($row['penalty_25']) . "\n");
-            $pdf->Write(0, "Interest: " . strval($row['interest_2']) . "\n");
-            $pdf->Write(0, "OR Number: " . $row['or_no'] . "\n");
-            $pdf->Write(0, "Date: " . $row['date'] . "\n");
-            $pdf->Write(0, "Total Amount: " . strval($row['total_amount']) . "\n");
-            $pdf->Write(0, "Remarks: " . $row['remarks'] . "\n");
-            $pdf->Write(0, "Owner: " . $row['fullname'] . "\n");
-            $pdf->Write(0, "Stall Number: " . $row['owner_id'] . "\n\n"); // Add additional line break
-        }
-    } else {
-        $pdf->Write(0, 'No data found');
+    // Write data rows
+    while ($row = $result->fetch_assoc()) {
+        fputcsv($file, $row);
     }
 
-    // Close and output PDF
-    $pdf->Output('monthly_report.pdf', 'I');
-    }
+    // Close the database connection
+    $conn->close();
 
-    
+    // Close the file pointer
+    fclose($file);
+
+    // Set headers to force download the CSV file
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="data.csv"');
+    header('Content-Length: ' . filesize('data.csv'));
+
+    // Output the file to the browser
+    readfile('data.csv');
+
+    // Delete the file after download
+    unlink('data.csv');
+    }
 ?>
